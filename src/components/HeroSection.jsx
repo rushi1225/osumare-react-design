786d43d0f74170d3a5f7702d53ef3d96b429acab
Rushikesh Bhingare <rushibhingare25@gmail.com>
2025-01-24 19:56:28 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -48,7 +48,7 @@ const HeroSection = () => {
                             <div className="modal-dialog modal-dialog-centered " role="document">
                                 <div className="modal-content">
                                     <div className="modal-header">
-                                        <h5 className="modal-title" id="exampleModalLongTitle">Get Started Today</h5>
+                                        <h5 className="modal-title" id="exampleModalLongTitle">Get Started</h5>
                                         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
